647e3f3485cc775da3aef2a0756dfbc8d575de69
Kjell <kjell@leanside.com>
2012-11-06 01:04:00 -0600

    Tests for #start and #info

--- spec/pomux_spec.rb
+++ spec/pomux_spec.rb
@@ -13,4 +13,21 @@ describe Pomux do
   it "should write ~/.pomux to spec/.pomux when testing" do
     File.exists?(path).should be_true
   end
+
+  describe "#start" do
+    subject { pomux.start; pomux }
+
+    its(:started) { should be_within(5).of(Time.now) }
+    its(:count) { should == 0 }
+    its(:ended) { should_not be_within(60*30).of(Time.now) } # TODO: Bad test. Write the pomux fixture in setup instead of copying one with the same ended:
+  end
+
+  describe "#info" do
+    subject { pomux.info }
+    it "should track :started, :count, and :last" do
+      pomux.info['started'].should be_nil
+      pomux.info['count'].should == 0
+      pomux.info['last'].should be_a_kind_of(Time)
+    end
+  end
 end
